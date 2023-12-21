cdb8ff72268de6b310471e7ca66ea586bc9ddd7d
putilovskaya <putilovskayaa@mail.ru>
2023-12-21 22:08:28 +0300

    add subtract_number to funcs.py

--- funcs.py
+++ funcs.py
@@ -3,3 +3,11 @@ def add_number(a, b):
     Прибавляет к числу  a чило b
     """
     return a + b
+
+
+def subtract_number(a, b):
+    """
+    Вычитает из числа a число b
+    """
+    return a - b
+
